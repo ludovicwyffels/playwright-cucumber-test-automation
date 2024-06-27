e0e2dfed60ab385176d5795691d49aed239c3c83
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2024-06-27 19:16:10 +0200

    init

---
+++ cucumber.mjs
@@ -0,0 +1,33 @@
+import path from "node:path";
+
+const getWorldParams = () => {
+    const params = {
+        foo: 'bar',
+    };
+
+    return params;
+};
+
+const config = {
+    requireModule: ['ts-node/register'],
+    require: ['src/**/*.ts'],
+    format: [
+        // 'message:e2e/reports/cucumber-report.ndjson',
+        'json:reports/cucumber-report.json',
+        'html:reports/report.html',
+        'summary',
+        'progress-bar',
+    ],
+    formatOptions: { snippetInterface: 'async-await' },
+    worldParameters: getWorldParams(),
+};
+
+if (process.env.USE_ALLURE) {
+    //config.format.push('./src/support/reporters/allure-reporter.ts');
+    config.format.push(
+        path.resolve(__dirname, './src/support/reporters/allure-reporter.ts')
+    )
+} else {
+    config.format.push('@cucumber/pretty-formatter');
+}
+export default config;
